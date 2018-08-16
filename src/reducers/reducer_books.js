9355a2b1aa4c11469b093b24b35ac84381653e7a
matt_hoffman <matthoffman@enphaseenergy.com>
2018-08-15 19:58:17 -0700

    lesson 47

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Daring Faith'},
-    {title: 'Practical Java'},
-    {title: 'Everything You Wanted to Know About ?'},
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: 'Daring Faith', pages: 39},
+    {title: 'Practical Java', pages: 101},
+    {title: 'Everything You Wanted to Know About ?', pages: 123},
   ]
 }

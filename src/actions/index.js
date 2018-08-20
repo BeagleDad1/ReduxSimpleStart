b138b1d329072d5744f8ec962d504c8d666886ef
CodeWarrior <formatt.hoffman@gmail.com>
2018-08-19 19:43:37 -0700

    lesson 59

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+import axios from 'axios'
 // openweathermap.org api key
 // DON'T check in to github I have made it blank
 const API_KEY = '';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+  const url =`${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}

ef44c1cf7c2c69003fecda1bdbb216293dfa68d3
CodeWarrior <formatt.hoffman@gmail.com>
2018-08-31 08:01:26 -0700

    removed api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // openweathermap.org api key
 // DON'T check in to github I have made it blank
-const API_KEY = '948bac555a57bdb6a7e5f2b3f2abfb8d';
+const API_KEY = '';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 

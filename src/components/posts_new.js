0cd3b684a1524f34aa7dee98c0e7392cd751dd3e
matt_hoffman <matthoffman@enphaseenergy.com>
2018-09-04 14:49:09 -0700

    lesson 90

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return(
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                 />
+                 {field.meta.error}
+            </div>
+        );
+
+    }
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+            name='title'
+            label='Title for Post'
+            component={this.renderField}
+         />
+         <Field
+             name='categories'
+             label='Categories'
+             component={this.renderField}
+          />
+          <Field
+              name='content'
+              label='Post Content'
+              component={this.renderField}
+           />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    // Validate the inputs from 'values'
+    if(!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if(!values.categories) {
+        errors.categories = "Enter some Categories";
+    }
+    if(!values.content) {
+        errors.content = "Enter some content please";
+    }
+    // If errors is empty the form is fine to submit
+    // If errors has any properties, redux form assumes form is invalid.
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
